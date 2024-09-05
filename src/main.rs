40df4296397b36cc039b6d37007c8da1d251c845
Carson <carsonbeck2018@gmail.com>
2024-09-05 14:14:22 -0400

    Slash command test

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,38 @@
-use anyhow::Context as _;
-use serenity::async_trait;
-use serenity::model::channel::Message;
-use serenity::model::gateway::Ready;
-use serenity::prelude::*;
-use shuttle_runtime::SecretStore;
-use tracing::{error, info};
+use poise::serenity_prelude as serenity;
 
-struct Bot;
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
 
-#[async_trait]
-impl EventHandler for Bot {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "~freaky" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "https://tenor.com/view/freakbob-gif-11176155028712317218").await {
-                error!("Error sending message: {:?}", e);
-            }
-        }
-    }
-
-    async fn ready(&self, _: Context, ready: Ready) {
-        info!("{} is connected!", ready.user.name);
-    }
+/// Displays your or another user's account creation date
+#[poise::command(slash_command, prefix_command)]
+async fn age(ctx: Context<'_>, #[description = "Selected user"] user: Option<serenity::User>, ) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    ctx.say(response).await?;
+    Ok(())
 }
 
-#[shuttle_runtime::main]
-async fn serenity(#[shuttle_runtime::Secrets] secrets: SecretStore,) -> shuttle_serenity::ShuttleSerenity {
-    // Get the discord token set in `Secrets.toml`
-    let token = secrets
-        .get("DISCORD_TOKEN")
-        .context("'DISCORD_TOKEN' was not found")?;
+#[tokio::main]
+async fn main() {
+    let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
+    let intents = serenity::GatewayIntents::non_privileged();
 
-    // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![age()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
 
-    let client = Client::builder(&token, intents)
-        .event_handler(Bot)
-        .await
-        .expect("Err creating client");
-
-    Ok(client.into())
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+    client.unwrap().start().await.unwrap();
 }
